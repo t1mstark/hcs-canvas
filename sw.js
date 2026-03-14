3bce3efbafc452c0ecb7ba53a53d2717069fa69d
t1mstark <t1111m.johann.stark@gmail.com>
2026-03-14 21:05:59 +0100

    Initial HCS Canvas v1

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='hcs-canvas-v1';
+const ASSETS=['/','/index.html','/style.css','/app.js'];
+self.addEventListener('install',(e)=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',(e)=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',(e)=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>hit)));
+});
