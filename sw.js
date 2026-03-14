c112621a4908f4432b7e28389fdf277b9fab19e9
t1mstark <t1111m.johann.stark@gmail.com>
2026-03-14 21:08:48 +0100

    Fix GitHub Pages 404: use relative asset paths

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='hcs-canvas-v1';
-const ASSETS=['/','/index.html','/style.css','/app.js'];
+const ASSETS=['./','./index.html','./style.css','./app.js'];
 self.addEventListener('install',(e)=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',(e)=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',(e)=>{
